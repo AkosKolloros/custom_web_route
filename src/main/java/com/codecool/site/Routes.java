932ece0fe0a6f301748a48370a07491ed55eef47
Akos Kolloros <kollorosa@gmail.com>
2019-03-04 17:58:31 +0100

    users route

--- src/main/java/com/codecool/site/Routes.java
+++ src/main/java/com/codecool/site/Routes.java
@@ -1,6 +1,15 @@
 package com.codecool.site;
 
+import static com.codecool.site.RequestMethod.POST;
+
 public class Routes {
+
+    @WebRoute("/")
+    public String index(){
+        // Here goes the code to handle all requests going to localhost:8000/test1
+        // and to return something
+        return "Hello! This is the index";
+    }
 
     @WebRoute("/test1")
     public String test1(){
@@ -14,5 +23,12 @@ public class Routes {
         // Here goes the code to handle all requests going to localhost:8000/test2
         // and to return something
         return "This is also my webpage";
+    }
+
+    @WebRoute(method=POST, path = "/users")
+    public String users() {
+        // here goes the code to handle POST requests going to localhost:8000/users
+
+        return "This is the users page";
     }
 }
