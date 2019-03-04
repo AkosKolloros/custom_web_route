a753479ca2098a0c67ab39407d900ca379e5042b
Akos Kolloros <kollorosa@gmail.com>
2019-03-04 17:40:04 +0100

    done assignment

--- src/main/java/com/codecool/site/Test.java
+++ src/main/java/com/codecool/site/Test.java
@@ -2,6 +2,7 @@ package com.codecool.site;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
 
 import com.sun.net.httpserver.HttpExchange;
@@ -11,21 +12,40 @@ import com.sun.net.httpserver.HttpServer;
 public class Test {
 
     public static void main(String[] args) throws Exception {
-
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/test", new MyHandler());
+
+        for (Method m : Routes.class.getMethods()) {
+            Class<WebRoute> webRouteClass = WebRoute.class;
+            if (m.isAnnotationPresent(webRouteClass)) {
+                /*
+                Here comes your logic.
+                If the given path from the HttpExchange method is the SAME like the WebRoute annotation's path, you should INVOKE this method.
+                */
+
+                String route = m.getAnnotation(webRouteClass).value();
+
+                String response = m.invoke(new Routes()).toString();
+
+                server.createContext(route, new MyHandler(response));
+            }
+        }
+
         server.setExecutor(null); // creates a default executor
         server.start();
     }
 
     static class MyHandler implements HttpHandler {
+        private String response;
+
         public void handle(HttpExchange t) throws IOException {
-            String response = "This is the response";
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
             os.close();
         }
-    }
 
+        public MyHandler(String response) {
+            this.response = response;
+        }
+    }
 }
